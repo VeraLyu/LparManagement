cb2841c4d9b82088b1e58feb7ea04481398bab61
Royce Lyu <lvroyce0210@gmail.com>
2015-03-02 14:18:56 +0800

    GA1
    Signed-off-by: Wen Ruo Lyu <lvroyce@linux.vnet.ibm.com>

--- staticfiles/js/countdown.js
+++ staticfiles/js/countdown.js
@@ -1,4 +1,43 @@
+function get_lefttime(tm) {
+    var rsv_hours = parseInt(tm[0]) + 4;
+    var rsv_minutes = parseInt(tm[1]);
+    var rsv_seconds = parseInt(tm[2]);
+    var date = new Date();
+    var cur_hours = date.getHours();
+    var cur_minutes = date.getMinutes();
+    var cur_seconds = date.getSeconds();
+    var total_seconds = (rsv_hours - cur_hours) * 3600 + (rsv_minutes - cur_minutes) * 60 + (rsv_seconds - cur_seconds)
+    return total_seconds;
+}
+
+function check_time(time) {
+    if (time < 10)
+        return '0' + String(time)
+    else
+        return String(time)
+}
+
+function show_time(sp, seconds) {
+    hours = Math.floor(seconds / 3600);
+    minutes = Math.floor((seconds % 3600) / 60);
+    seconds = seconds % 60;
+    hours = check_time(hours);
+    minutes = check_time(minutes);
+    seconds = check_time(seconds);
+    time = hours + ':' + minutes + ':' + seconds;
+    sp.innerHTML = time;
+}
+
 function countdown() {
-  var target = document.getElementById('LPAR10');
-  alert(target.value)
+    lpars = new Array();
+    for(i = 0; i <= 30; i++) {
+        lpars[i] = 'LPAR' + i;
+        var sp = document.getElementById(lpars[i]);
+        if(!sp)
+            continue;
+        tm = sp.innerHTML.split(':');
+        seconds = get_lefttime(tm);
+        if (seconds >= 0)
+            show_time(sp, seconds);
+    }
 }
